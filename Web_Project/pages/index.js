5a1f1ddc33ee625f65385b2701470fe19c8a835b
MultiverseDestroyer <vickmokale556@gmail.com>
2024-11-25 07:59:18 +0800

    A step ahead of conquering this repository
    Gib me ur acount

---
+++ Web_Project/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+      <div>
+          <pre className={styles.codeBlock}>
+              console.log('Hello, Worlds!');
+              console.log('Hello, Evan qt ini Ralfy');
+              console.log('\n\nBow down thy thee to the ultimate multiverse conquer and show your utterly respect for me you peasant\n\n- Sincerely your supreme leader, MultiverseDestroyer.')
+          </pre>
+      </div>
+  );
+}

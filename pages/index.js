6e80d8a3ccb005ca4ac905d136b6378f3e2d50b2
evmmmm <evanmokale556@gmail.com>
2024-11-24 22:42:00 +0800

    Bug killing

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,9 @@ import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
       <div>
-          <h1 className={styles.heading}>Welcome to My Dynamic Website!</h1>
-          <p>Here is some data fetched from the server:</p>
+          <pre className={styles.codeBlock}>
+              console.log('Hello, World!');
+          </pre>
       </div>
   );
 }

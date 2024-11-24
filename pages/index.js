9fbcb62307f67ebbac1a6b650d56437b7adbdf8b
evmmmm <evanmokale556@gmail.com>
2024-11-24 22:37:11 +0800

    Buggggg fixing

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import styles from '../styles/Home.module.css';
 
-export default function Home({ data }) {
+export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Welcome to My Dynamic Website!</h1>
-      <p>Here is some data fetched from the server:</p>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
+      <div>
+          <h1 className={styles.heading}>Welcome to My Dynamic Website!</h1>
+          <p>Here is some data fetched from the server:</p>
+      </div>
   );
 }

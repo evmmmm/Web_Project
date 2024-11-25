5fde5bdcf742c4286a494dea0726645d19602533
evmmmm <evanmokale556@gmail.com>
2024-11-25 20:52:28 +0800

    Memperbaiki tampilan

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,8 @@ export default function Home() {
   return (
       <div>
           <pre className={styles.codeBlock}>
-              console.log('Hello, Worlds!');
-              console.log('Hello, Evan qt ini Ralfy');
-              console.log('\n\nBow down thy thee to the ultimate multiverse conquer and show your utterly respect for me you peasant\n\n- Sincerely your supreme leader, MultiverseDestroyer.')
+                console.log('Hello, Worlds!');
+                console.log('Hello, Test');
           </pre>
       </div>
   );

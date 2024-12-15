f82c72168f5c94289c486659798a6b3d6fea5a42
evmmmm <evanmokale556@gmail.com>
2024-12-15 16:15:33 +0800

    The two of them stared in agreement at each other as they both knew what will the very next thing they will create.

--- frontend/src/pages/FindAccount.jsx
+++ frontend/src/pages/FindAccount.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';  // Pastikan Link diimpor di sini
 
 const FindAccount = () => {
   const [email, setEmail] = useState('');
